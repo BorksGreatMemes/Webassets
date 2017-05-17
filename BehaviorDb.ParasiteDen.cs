ae6906d12004383d9477d3addda9c1e3c0b8a28d
Bork <brandon.thomas.lum@gmail.com>
2017-05-17 07:28:46 -0400

    Create BehaviorDb.ParasiteDen.cs

---
+++ BehaviorDb.ParasiteDen.cs
@@ -0,0 +1,384 @@
+  /* <Object type="0x7fc2" id="Shadow Magus">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars16x16ParasiteDen</File>
+      <Index>0x00</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>130</Size>
+    <MaxHitPoints>300000</MaxHitPoints>
+    <Defense>35</Defense>
+    <Hero/>
+    <Quest/>
+    <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Dragon Eye Laser</ObjectId>
+      <Damage>135</Damage>
+      <Speed>80</Speed>
+      <LifetimeMS>2000</LifetimeMS>
+      <Size>100</Size>
+    </Projectile>
+    <Projectile id="1">
+      <ObjectId>Purple Star</ObjectId>
+      <Speed>100</Speed>
+      <Damage>70</Damage>
+      <Size>100</Size>
+      <LifetimeMS>2000</LifetimeMS>
+      <ConditionEffect duration="3">Confused</ConditionEffect>
+    </Projectile>
+    <Projectile id="2">
+      <ObjectId>Dragon Eye Blast</ObjectId>
+      <Damage>150</Damage>
+      <Speed>70</Speed>
+      <LifetimeMS>2000</LifetimeMS>
+      <Size>110</Size>
+      <MultiHit/>
+    </Projectile>
+    <Projectile id="3">
+      <ObjectId>Purple Mystic Shot</ObjectId>
+      <Damage>220</Damage>
+      <Speed>50</Speed>
+      <LifetimeMS>2000</LifetimeMS>
+      <Size>120</Size>
+      <Amplitude>1</Amplitude>
+      <Frequency>1</Frequency>
+      <MultiHit/>
+    </Projectile>
+  </Object>
+  
+   <Object type="0x7fc7" id="Parasite Colony">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars16x16ParasiteDen</File>
+      <Index>0x01</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>100</Size>
+    <MaxHitPoints>10000</MaxHitPoints>
+    <Defense>200</Defense>
+    <Hero/>
+    <Quest/>
+     <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Oryx Insect Sting Status</ObjectId>
+      <Damage>70</Damage>
+      <Speed>50</Speed>
+      <LifetimeMS>800</LifetimeMS>
+      <Size>90</Size>
+    </Projectile>
+  </Object>
+  
+  <Object type="0x7fd5" id="Controlled Wizzard">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars8x8ParasiteDen</File>
+      <Index>0x03</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>100</Size>
+    <MaxHitPoints>6000</MaxHitPoints>
+    <Defense>20</Defense>
+    <Hero/>
+    <Quest/>
+    <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Orange Magic</ObjectId>
+      <MinDamage>45</MinDamage>
+	    <MaxDamage>120</MaxDamage>
+      <Speed>120</Speed>
+      <LifetimeMS>800</LifetimeMS>
+      <Size>100</Size>
+     <Amplitude>0.5</Amplitude>
+	   <Frequency>2</Frequency>
+    </Projectile>
+  <Projectile id="1">
+      <ObjectId>Skull Wave</ObjectId>
+      <MinDamage>53</MinDamage>
+	    <MaxDamage>115</MaxDamage>
+      <Speed>50</Speed>
+      <LifetimeMS>2300</LifetimeMS>
+      <Size>100</Size>
+     <Amplitude>0.5</Amplitude>
+	   <Frequency>2</Frequency>
+    </Projectile>
+  </Object>
+
+  
+  <Object type="0x7fd6" id="Controlled Warrior">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars8x8ParasiteDen</File>
+      <Index>0x01</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>100</Size>
+    <MaxHitPoints>7000</MaxHitPoints>
+    <Defense>50</Defense>
+    <Hero/>
+    <Quest/>
+    <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Purple Bolt</ObjectId>
+	    <Damage>130</Damage>
+      <Speed>120</Speed>
+      <LifetimeMS>800</LifetimeMS>
+      <Size>100</Size>
+    </Projectile>
+  </Object>
+
+   <Object type="0x7fd7" id="Parasidic Blob">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars8x8ParasiteDen</File>
+      <Index>0x00</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>100</Size>
+    <MaxHitPoints>680</MaxHitPoints>
+    <Defense>60</Defense>
+    <Hero/>
+    <Quest/>
+     <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Directed Explosion</ObjectId>
+	    <Damage>5</Damage>
+      <Speed>200</Speed>
+      <LifetimeMS>5</LifetimeMS>
+      <Size>100</Size>
+     <ArmorPiercing/>
+    </Projectile>
+  </Object>
+  
+   <Object type="0x7fd8" id="Nightmare Maw">
+    <Group>Heros</Group>
+    <Enemy/>
+    <Class>Character</Class>
+    <AnimatedTexture>
+      <File>chars16x16ParasiteDen</File>
+      <Index>0x02</Index>
+    </AnimatedTexture>
+    <HitSound>monster/cubes_hit</HitSound>
+    <DeathSound>monster/cubes_death</DeathSound>
+    <Size>100</Size>
+    <MaxHitPoints>6800</MaxHitPoints>
+    <Defense>60</Defense>
+    <Hero/>
+    <Quest/>
+     <Level>10</Level>
+    <XpMult>0.0</XpMult>
+    <Projectile id="0">
+      <ObjectId>Coffin Bullet</ObjectId>
+	    <Damage>55</Damage>
+      <Speed>70</Speed>
+      <LifetimeMS>1000</LifetimeMS>
+      <Size>100</Size>
+     <ArmorPiercing/>
+    </Projectile>
+   <Projectile id="1">
+      <ObjectId>Fire Claw</ObjectId>
+	    <Damage>90</Damage>
+      <Speed>75</Speed>
+      <Size>100</Size>
+      <LifetimeMS>350</LifetimeMS>
+      <ConditionEffect duration="2">Slowed</ConditionEffect>
+    </Projectile>
+  </Object>
+  
+  */
+  
+  
+  
+  
+  
+  
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using wServer.realm;
+using wServer.logic.behaviors;
+using wServer.logic.loot;
+using wServer.logic.transitions;
+
+namespace wServer.logic
+{
+    partial class BehaviorDb
+    {
+        _ ParasiteDen = () => Behav()
+             .Init("Shadow Magus",
+                new State(
+                    new State("Waitforplayers",
+                    new ConditionalEffect(ConditionEffectIndex.Invincible, true),
+                    new PlayerWithinTransition(10, "Taunt1")
+                    ),
+                    new State("Taunt1",
+                    new Taunt(true, "How peculiar..."),
+                    new TimedTransition(5000, "Taunt2")
+                    ),
+                    new State("Taunt2",
+                    new Taunt(true, "How are you, {PLAYER}. A mortal alive in the parasite den?"),
+                    new TimedTransition(5000, "Taunt3")
+                    ),
+                    new State("Taunt3",
+                    new Taunt(true, "You may have survived the creatures that lie here... But you won't get past me and my spells!"),
+                    new TimedTransition(5000, "Taunt4")
+                    ),
+                    new State("Taunt4",
+                    new Flash(0xff0000, 1, 20),
+                    new ConditionalEffect(ConditionEffectIndex.Invincible),
+                    new Taunt(true, "It's time you learned your place... I will eradicate you vermin from this realm!"),
+                    new TimedTransition(5000, "Phase1/2")
+                    ),
+                    new State("Phase1/2",
+                    new Shoot(10, count: 4, shootAngle: 90, predictive: 0.5, projectileIndex: 0, coolDown: 500),
+                    new Shoot(10, count: 8, shootAngle: 45, predictive: 0.5, projectileIndex: 1, coolDown: 555, coolDownOffset: 1500),
+                    new Shoot(10, count: 3, shootAngle: 35, predictive: 0.5, projectileIndex: 2, coolDown: 1000),
+                    new HpLessTransition(0.70, "POD"),
+                    new TimedTransition(12000, "Phase2/2")
+                    ),
+                    new State("Phase2/2",
+                    new Shoot(10, count: 1, projectileIndex: 0, coolDown: 5),
+                    new Shoot(10, count: 1, projectileIndex: 0, coolDown: 5, coolDownOffset: 10),
+                    new Shoot(10, count: 1, projectileIndex: 0, coolDown: 5),
+                    new Shoot(10, count: 1, projectileIndex: 0, coolDown: 5, coolDownOffset: 10),
+                    new Shoot(10, count: 8, shootAngle: 45, projectileIndex: 1, coolDown: 500, coolDownOffset: 500),
+                    new HpLessTransition(0.70, "POD"),
+                    new TimedTransition(7000, "Phase1/2")
+                    ),
+                    new State("POD",
+                    new ConditionalEffect(ConditionEffectIndex.Invincible),
+                    new Taunt(true, "Ancient spell... Tornado"),
+                    new TimedTransition(5000, "AST")
+                    ),
+                    new State("AST",
+                    new ConditionalEffect(ConditionEffectIndex.Armored),
+                    new Shoot(20, count: 8, projectileIndex: 3, fixedAngle: 0, coolDown: 5),
+                    new Shoot(20, count: 8, projectileIndex: 3, fixedAngle: 0, coolDown: 5, coolDownOffset: 10),
+                    new TimedTransition(10000, "BreakAST")
+                    ),
+                    new State("BreakAST",
+                    new Shoot(10, count: 8, shootAngle: 45, coolDown: 500, coolDownOffset: 500),
+                    new TimedTransition(5000, "AST"),
+                    new HpLessTransition(0.45, "Phase2-1/3")
+                    ),
+                    new State("Phase2-1/3",
+                    new Shoot(10, count: 8, shootAngle: 45, coolDown: 500, coolDownOffset: 500)
+                    )
+                    ),
+                new TierLoot(10, ItemType.Weapon, 0.4),
+                new TierLoot(11, ItemType.Weapon, 0.2),
+                new TierLoot(12, ItemType.Weapon, 0.1),
+                new TierLoot(10, ItemType.Armor, 0.4),
+                new TierLoot(11, ItemType.Armor, 0.2),
+                new TierLoot(12, ItemType.Armor, 0.1),
+                new TierLoot(5, ItemType.Ring, 0.15),
+                new TierLoot(6, ItemType.Ring, 0.009),
+                new Threshold(0.18,
+                    new ItemLoot("Potion of Life", 1)
+                    )
+            )
+        .Init("Parasite Colony",
+                new State(
+                    new State("Waitforplayers",
+                    new ConditionalEffect(ConditionEffectIndex.Invincible),
+                    new PlayerWithinTransition(10, "Wander")
+                    ),
+                    new State("Wander",
+                    new StayCloseToSpawn(0.8, 5),
+                    new Wander(0.1),
+                    new Shoot(10, count: 20, shootAngle: 18, predictive: 0.5, projectileIndex: 0, coolDown: 1500),
+                    new TimedTransition(10000, "Spawn")
+                    ),
+                    new State("Spawn",
+                    new Spawn("Controlled Wizzard", maxChildren: 1, initialSpawn: 1, coolDown: 1000000000),
+                    new Spawn("Controlled Warrior", maxChildren: 1, initialSpawn: 1, coolDown: 1000000000),
+                    new TimedTransition(500, "Wander")
+                       )
+                     )
+                   )
+         .Init("Controlled Wizzard",
+                new State(
+                    new State("Attack",
+                    new Wander(0.1),
+                    new Shoot(10, count: 2, shootAngle: 10, predictive: 0.5, projectileIndex: 0, coolDown: 500),
+                    new TimedTransition(6000, "Spell Bomb")
+                    ),
+                    new State("Spell Bomb",
+                    new Shoot(10, count: 20, shootAngle: 18, predictive: 0.5, projectileIndex: 1, coolDown: 1500),
+                    new TimedTransition(500, "Attack")
+                         )
+                      )
+                  )
+        .Init("Controlled Warrior",
+                new State(
+                    new State("Attack",
+                    new Wander(0.1),
+                    new Shoot(10, count: 1, predictive: 0.5, projectileIndex: 0, coolDown: 1300),
+                    new TimedTransition(6000, "Jugg")
+                    ),
+                    new State("Jugg",
+                    new ConditionalEffect(ConditionEffectIndex.Armored),
+                    new Shoot(10, count: 1, predictive: 0.5, projectileIndex: 0, coolDown: 650),
+                    new TimedTransition(4500, "Attack")
+                        )
+                    )
+                )
+        .Init("Parasidic Blob",
+                new State(
+                    new State("Attack",
+                    new Wander(0.1),
+                    new Follow(1.5, range: 1, duration: 5000, coolDown: 0),
+                    new Shoot(10, count: 8, predictive: 0.5, shootAngle: 45, projectileIndex: 0, coolDown: 5)
+                         )
+                      )
+                   )
+        .Init("Nightmare Maw",
+                new State(
+                    new State("Attack",
+                    new Wander(0.1),
+                    new Shoot(10, count: 3, predictive: 0.5, projectileIndex: 0, coolDown: 1700),
+                    new TimedTransition(10000, "CPClamp")
+                    ),
+                    new State("CPClamp",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable),
+                    new Flash(0xff0000, 1, 20),
+                    new TimedTransition(3000, "Clamp")
+                    ),
+                    new State("Clamp",
+                    new Follow(1.3, range: 1, duration: 5000, coolDown: 0),
+                    new Shoot(10, count: 20, shootAngle: 18, coolDown: 99999, projectileIndex: 1, coolDownOffset: 5),
+                    new Shoot(10, count: 20, shootAngle: 18, coolDown: 99999, projectileIndex: 1, coolDownOffset: 220),
+                    new Shoot(10, count: 20, shootAngle: 18, coolDown: 99999, projectileIndex: 1, coolDownOffset: 420), 
+                    new Shoot(10, count: 20, shootAngle: 18, coolDown: 99999, projectileIndex: 1, coolDownOffset: 620),
+                    new TimedTransition(800, "Attack")
+                    )
+                )
+            );
+    }
+}
+  
+  
+  
+  
+  
